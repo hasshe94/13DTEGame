ef00f23fc340e6b9acfd1e2999d908ae8c9546a5
hasshe94 <hasshe94@kingshigh.school.nz>
2024-05-28 16:40:16 +1200

    Character animations made and folders sorted

--- 13HasanSheikhGame/Scripts/player.gd
+++ 13HasanSheikhGame/Scripts/player.gd
@@ -16,6 +16,39 @@ var anim_state = state.IDLE
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func update_state():
+	if anim_state == state.HURT:
+		return
+	if is_on_floor():
+		if velocity == Vector2.ZERO:
+			anim_state = state.IDLE
+		elif velocity.x !=0:
+			anim_state = state.RUNNING
+	else:
+		if velocity.y < 0:
+			anim_state = state.JUMPUP
+		else:
+			anim_state = state.JUMPDOWN
+
+func update_animation(direction):
+	if direction > 0:
+		animator.flip_h = false
+	elif direction < 0:
+		animator.flip_h = true
+	match anim_state:
+		state.IDLE:
+			animation_player.play("idle")
+		state.RUNNING:
+			animation_player.play("run")
+		state.JUMPUP:
+			animation_player.play("jump_up")
+		state.JUMPDOWN:
+			animation_player.play("jump_down")
+		state.HURT:
+			animation_player.play("hurt")
+		
+	
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -32,6 +65,8 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = move_toward(velocity.x,direction*speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-
+		velocity.x = move_toward(velocity.x, 0, acceleration)
+	
+	update_state()
+	update_animation(direction)
 	move_and_slide()
