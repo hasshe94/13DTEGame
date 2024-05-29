a2557c5641d592ffb80364632ef485426e310d38
hasshe94 <hasshe94@kingshigh.school.nz>
2024-05-29 12:23:10 +1200

    Made a collectable item, built the first level and added a basic UI

---
+++ 13HasanSheikhGame/Scripts/artifact.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+
+func collected():
+	var vanish = preload("res://Scenes/vanish.tscn")
+	var new_vanish_object = vanish.instantiate()
+	get_tree().current_scene.add_child(new_vanish_object)
+	new_vanish_object.global_position = global_position
+	GameManager.score += 1
+	
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		collected()
+		queue_free()
