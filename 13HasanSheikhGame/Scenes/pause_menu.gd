c37ea076ba949684ba7ef578f39fc2bd3190f15e
hasshe94 <hasshe94@kingshigh.school.nz>
2024-06-06 09:57:57 +1200

    Pause Menu Essentially done, just need to fix one bug

---
+++ 13HasanSheikhGame/Scenes/pause_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var World = $".../.../.../.../.../.../"
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_resume_pressed():
+	World.pauseMenu()
+	
