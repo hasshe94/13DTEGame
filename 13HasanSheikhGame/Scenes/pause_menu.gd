7d4dc358aa7c9c0e60dd6378d504ea73ba8086d2
hasshe94 <hasshe94@kingshigh.school.nz>
2024-06-07 10:26:55 +1200

    Pause menu bux fix done

--- 13HasanSheikhGame/Scenes/pause_menu.gd
+++ 13HasanSheikhGame/Scenes/pause_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var World = $".../.../.../.../.../.../"
+@onready var World = get_node("/root/World")
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
