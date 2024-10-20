dea476788af4db40900b74f4473eac02926992d8
hasshe94 <hasshe94@kingshigh.school.nz>
2024-10-20 14:58:33 +1300

    Fixing spike trap
    Spike trap needs different collision shape per animation so player can pass without taking damage

--- 13HasanSheikhGame/Scripts/artifact.gd
+++ 13HasanSheikhGame/Scripts/artifact.gd
@@ -15,7 +15,7 @@ func collected():
 	new_vanish_object.global_position = global_position
 	GameManager.score += 1
 	if GameManager.score == 15:
-		get_tree().change_scene_to_file("res://Scenes/about.tscn")
+		get_tree().change_scene_to_file("res://Scenes/win_screen.tscn")
 
 
 func reset():
