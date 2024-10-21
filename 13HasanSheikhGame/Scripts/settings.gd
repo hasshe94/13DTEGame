c9cc0a2403fc618ea93d133b800775ede9433abd
hasshe94 <hasshe94@kingshigh.school.nz>
2024-10-21 22:22:55 +1300

    Fixed double jump glitch, now fixing sound settings tab

--- 13HasanSheikhGame/Scripts/settings.gd
+++ 13HasanSheikhGame/Scripts/settings.gd
@@ -15,3 +15,7 @@ func _on_confirm_pressed():
 	AudioServer.set_bus_volume_db(0, linear_to_db($AudioOptions/VBoxContainer/MasterSlider.value))
 	AudioServer.set_bus_volume_db(1, linear_to_db($AudioOptions/VBoxContainer/SFXSlider.value))
 	AudioServer.set_bus_volume_db(2, linear_to_db($AudioOptions/VBoxContainer/MusicSlider.value))
+
+
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/about.tscn")
